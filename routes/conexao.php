656f88b3e12a12e766791ba29788233a0940c377
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2021-07-19 18:00:35 -0300

    chore: excluindo dados sensiveis

--- routes/conexao.php
+++ routes/conexao.php
@@ -1,8 +1,8 @@
 <?php
     $servidor   = "localhost";
-    $usuario    = "vmamco72_wp3";
-    $senha      = "WYFNpLkP5M7qGxDA";
-    $dbname     = "vmamco72_wp3";
+    $usuario    = "#";
+    $senha      = "#";
+    $dbname     = "#";
 
     $conn       = mysqli_connect($servidor, $usuario, $senha, $dbname);
 ?>
